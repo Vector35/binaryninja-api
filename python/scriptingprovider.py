96acbed85902d8dfd43ef624afac72145ae6e577
Peter LaFosse <peter@vector35.com>
2017-01-03 16:30:25 -0500

    Reverting last commit, we can't use this method in the core, since it will break those launching binaryninja from the commandline

--- python/scriptingprovider.py
+++ python/scriptingprovider.py
@@ -634,8 +634,7 @@ class PythonScriptingProvider(ScriptingProvider):
 
 
 PythonScriptingProvider().register()
-if not sys.stdin.isatty():
-	# Wrap stdin/stdout/stderr for Python scripting provider implementation
-	sys.stdin = _PythonScriptingInstanceInput(sys.stdin)
-	sys.stdout = _PythonScriptingInstanceOutput(sys.stdout, False)
-	sys.stderr = _PythonScriptingInstanceOutput(sys.stderr, True)
+# Wrap stdin/stdout/stderr for Python scripting provider implementation
+sys.stdin = _PythonScriptingInstanceInput(sys.stdin)
+sys.stdout = _PythonScriptingInstanceOutput(sys.stdout, False)
+sys.stderr = _PythonScriptingInstanceOutput(sys.stderr, True)

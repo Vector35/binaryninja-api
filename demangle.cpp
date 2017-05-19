b9b7de18397e5f32d49f36917dd2a9a5e9a487c1
Jordan Wiens <jordan@psifertex.com>
2017-05-19 12:08:04 -0400

    fix indentation

--- demangle.cpp
+++ demangle.cpp
@@ -4,48 +4,48 @@ using namespace std;
 
 namespace BinaryNinja
 {
-bool DemangleMS(Architecture* arch,
-                const std::string& mangledName,
-                Type** outType,
-                QualifiedName& outVarName)
-{
-	BNType* localType = nullptr;
-	char** localVarName = nullptr;
-	size_t localSize = 0;
-	if (!BNDemangleMS(arch->GetObject(), mangledName.c_str(), &localType, &localVarName, &localSize))
-		return false;
-	if (!localType)
-		return false;
-	*outType = new Type(localType);
-	for (size_t i = 0; i < localSize; i++)
+	bool DemangleMS(Architecture* arch,
+									const std::string& mangledName,
+									Type** outType,
+									QualifiedName& outVarName)
 	{
-		outVarName.push_back(localVarName[i]);
-		BNFreeString(localVarName[i]);
+		BNType* localType = nullptr;
+		char** localVarName = nullptr;
+		size_t localSize = 0;
+		if (!BNDemangleMS(arch->GetObject(), mangledName.c_str(), &localType, &localVarName, &localSize))
+			return false;
+		if (!localType)
+			return false;
+		*outType = new Type(localType);
+		for (size_t i = 0; i < localSize; i++)
+		{
+			outVarName.push_back(localVarName[i]);
+			BNFreeString(localVarName[i]);
+		}
+		delete [] localVarName;
+		return true;
 	}
-	delete [] localVarName;
-	return true;
-}
 
 
-bool DemangleGNU3(Ref<Architecture> arch,
-                const std::string& mangledName,
-                Type** outType,
-                QualifiedName& outVarName)
-{
-	BNType* localType;
-	char** localVarName = nullptr;
-	size_t localSize = 0;
-	if (!BNDemangleGNU3(arch->GetObject(), mangledName.c_str(), &localType, &localVarName, &localSize))
-		return false;
-	if (!localType)
-		return false;
-	*outType = new Type(localType);
-	for (size_t i = 0; i < localSize; i++)
+	bool DemangleGNU3(Ref<Architecture> arch,
+									const std::string& mangledName,
+									Type** outType,
+									QualifiedName& outVarName)
 	{
-		outVarName.push_back(localVarName[i]);
-		BNFreeString(localVarName[i]);
+		BNType* localType;
+		char** localVarName = nullptr;
+		size_t localSize = 0;
+		if (!BNDemangleGNU3(arch->GetObject(), mangledName.c_str(), &localType, &localVarName, &localSize))
+			return false;
+		if (!localType)
+			return false;
+		*outType = new Type(localType);
+		for (size_t i = 0; i < localSize; i++)
+		{
+			outVarName.push_back(localVarName[i]);
+			BNFreeString(localVarName[i]);
+		}
+		delete [] localVarName;
+		return true;
 	}
-	delete [] localVarName;
-	return true;
-}
 }

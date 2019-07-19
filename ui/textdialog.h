8cf1942611f7d3e517412e2062da2a69f514cd9f
Peter LaFosse <peter@vector35.com>
2019-07-19 13:46:49 -0400

    Fix case sensitivity issue with the rename dialog

--- ui/textdialog.h
+++ ui/textdialog.h
@@ -1,28 +1,30 @@
 #pragma once
 
 #include <QtWidgets/QDialog>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QLabel>
+
 #include "binaryninjaapi.h"
 #include "uitypes.h"
+#include "binaryninjaapi.h"
 
 class BINARYNINJAUIAPI TextDialog: public QDialog
 {
 	Q_OBJECT
-	QWidget* m_parent;
-	QString m_title;
-	QString m_msg;
-	QStringList m_options;
-	Qt::WindowFlags m_flags;
 	QString m_qSettingsListName;
 	int m_historySize;
 	QString m_historyEntry;
 	QString m_initialText;
+	QStringList m_historyEntries;
+	QLabel* m_messageText;
+	QComboBox* m_combo;
 
 public:
 	TextDialog(QWidget* parent, const QString& title, const QString& msg, const QString& qSettingsListName,
 		const std::string& initialText = "");
 	TextDialog(QWidget* parent, const QString& title, const QString& msg, const QString& qSettingsListName,
 		const QString& initialText);
-	QString getItem(bool& ok);
+	QString getItem();
 	void setInitialText(const std::string& initialText) { m_initialText = QString::fromStdString(initialText); }
 	void commitHistory();
 };

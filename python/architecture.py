26d2190f914976af216fa3512bfe638f34799b96
Robert Crandall <rcrandall@researchinnovations.com>
2020-01-30 12:24:01 -0500

    Fixed return type documentation for get_instruction_text()

--- python/architecture.py
+++ python/architecture.py
@@ -1455,15 +1455,15 @@ class Architecture(with_metaclass(_ArchitectureMetaClass, object)):
 
 	def get_instruction_text(self, data, addr):
 		"""
-		``get_instruction_text`` returns a list of InstructionTextToken objects for the instruction at the given virtual
-		address ``addr`` with data ``data``.
+		``get_instruction_text`` returns a tuple with the list of InstructionTextToken objects and the decoded \
+        instruction length for the instruction at the given virtual address ``addr`` with data ``data``.
 
 		.. note:: Architecture subclasses should implement this method.
 
 		:param str data: max_instruction_length bytes from the binary at virtual address ``addr``
 		:param int addr: virtual address of bytes in ``data``
-		:return: an InstructionTextToken list for the current instruction
-		:rtype: list(InstructionTextToken)
+		:return: a tuple of list(InstructionTextToken) and length of instruction decoded
+		:rtype: tuple(list(InstructionTextToken), int)
 		"""
 		return self.perform_get_instruction_text(data, addr)
 

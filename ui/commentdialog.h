297ea6f73fe5c482611cf4e0d72090df73d9c16f
Xusheng <xusheng@vector35.com>
2020-07-13 15:27:44 +0800

    make the comment dialog non-modal

--- ui/commentdialog.h
+++ ui/commentdialog.h
@@ -5,14 +5,21 @@
 #include "binaryninjaapi.h"
 #include "dialogtextedit.h"
 #include "uicontext.h"
+#include "uicomment.h"
 
 class BINARYNINJAUIAPI CommentDialog: public QDialog
 {
 	Q_OBJECT
 
 	DialogTextEdit* m_comment;
+	UIComment m_uicomment;
 
 public:
-	CommentDialog(QWidget* parent, const QString& comment = "");
-	QString getComment();
+	CommentDialog(QWidget* parent, const UIComment& comment);
+	QString getNewComment();
+	QString getCurrentComment();
+	const FunctionRef& getCommentBackingFunction();
+	const BinaryViewRef& getCommentBackingData();
+	UICommentType getCommentType();
+	uint64_t getCommentAddress();
 };
---
+++ ui/uicomment.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "binaryninjaapi.h"
+#include "uicontext.h"
+
+// these are only intended to be used for the ui
+typedef enum 
+{
+	FunctionComment,
+	AddressComment
+}UICommentType;
+
+class UIComment
+{
+    public:
+        UICommentType type;
+        FunctionRef func;
+        BinaryViewRef data;
+        uint64_t address;
+        QString content;
+
+        UIComment(  UICommentType type,
+                    FunctionRef func,
+                    uint64_t address,
+                    QString content):
+                    type(type), func(func),
+                    address(address), content(content)
+        {}
+
+        UIComment(  UICommentType type,
+                    BinaryViewRef data,
+                    uint64_t address,
+                    QString content):
+                    type(type), data(data),
+                    address(address), content(content)
+        {}
+        
+        UIComment(const UIComment& rhs)
+        {
+            type = rhs.type;
+            func = rhs.func;
+            data = rhs.data;
+            address = rhs.address;
+            content = rhs.content;
+        }
+};

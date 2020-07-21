c32df09b8bb6cd8c4760e6e1ea23f49b8e5572d8
verylazyguy <verylazyguy@hotmail.com>
2020-07-21 11:55:33 -0400

    - log_to_stdout when running in headless mode

--- python/__init__.py
+++ python/__init__.py
@@ -164,6 +164,8 @@ def _init_plugins():
 	global _plugin_init
 	if not _plugin_init:
 		core.BNInitCorePlugins()
+		if not core_ui_enabled():
+			log_to_stdout()
 		if not os.environ.get('BN_DISABLE_USER_PLUGINS'):
 			core.BNInitUserPlugins()
 		core.BNInitRepoPlugins()

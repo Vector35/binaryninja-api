4e32142e4fe0eaeac191514553e6f0ec48c12384
Josh Ferrell <josh@vector35.com>
2020-08-24 21:58:03 -0400

    rapidjson serialization

--- rapidjson/allocators.h
+++ rapidjson/allocators.h
@@ -64,19 +64,19 @@ public:
     static const bool kNeedFree = true;
     void* Malloc(size_t size) { 
         if (size) //  behavior of malloc(0) is implementation defined.
-            return std::malloc(size);
+            return je_malloc(size);
         else
             return NULL; // standardize to returning NULL.
     }
     void* Realloc(void* originalPtr, size_t originalSize, size_t newSize) {
         (void)originalSize;
         if (newSize == 0) {
-            std::free(originalPtr);
+            je_free(originalPtr);
             return NULL;
         }
-        return std::realloc(originalPtr, newSize);
+        return je_realloc(originalPtr, newSize);
     }
-    static void Free(void *ptr) { std::free(ptr); }
+    static void Free(void *ptr) { je_free(ptr); }
 };
 
 ///////////////////////////////////////////////////////////////////////////////

b946577b46bbd1c1120b7b346d037eb4750a9d9f
Xusheng <xusheng@vector35.com>
2020-09-01 16:23:22 +0800

    add InferArraySize()

--- ui/commands.h
+++ ui/commands.h
@@ -14,10 +14,11 @@ bool BINARYNINJAUIAPI inputNameForLocalVariable(QWidget* parent, BinaryViewRef d
 bool BINARYNINJAUIAPI inputNameForType(QWidget* parent, std::string& name, const QString& title = "Set Name",
 	const QString& msg = "Enter name:");
 
+bool BINARYNINJAUIAPI InferArraySize(TypeRef& type, size_t selectionSize);
 bool BINARYNINJAUIAPI askForNewType(QWidget* parent, BinaryViewRef data, FunctionRef func, const std::string& title,
 	TypeRef& type, BinaryNinja::QualifiedName& name);
 bool BINARYNINJAUIAPI inputNewType(QWidget* parent, BinaryViewRef data, FunctionRef currentFunction,
-	uint64_t currentAddr, HighlightTokenState& highlight);
+	uint64_t currentAddr, size_t selectionSize, HighlightTokenState& highlight);
 bool BINARYNINJAUIAPI createInferredMember(QWidget* parent, BinaryViewRef data, HighlightTokenState& highlight,
 	FunctionRef func, BNFunctionGraphType type);
 

30683cdc2892d7a1a8b6b9070fbbceb8981882a8
Jordan Wiens <jordan@psifertex.com>
2020-12-22 11:51:24 -0500

    disable including rst source files in sphinx docs

--- api-docs/source/conf.py
+++ api-docs/source/conf.py
@@ -255,7 +255,8 @@ html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
 #
-html_show_sourcelink = True
+html_copy_source = False
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 #

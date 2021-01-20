afbc4ebe871b00b17984da82982bfc402aa4c784
Glenn Smith <glenn@vector35.com>
2021-01-20 16:38:00 -0500

    Catch main thread callback errors

--- mainthread.cpp
+++ mainthread.cpp
@@ -7,6 +7,7 @@ using namespace std;
 struct MainThreadActionContext
 {
 	function<void()> action;
+	exception_ptr exception;
 };
 
 
@@ -53,7 +54,26 @@ void BinaryNinja::RegisterMainThread(MainThreadActionHandler* handler)
 static void ExecuteAction(void* ctxt)
 {
 	MainThreadActionContext* action = (MainThreadActionContext*)ctxt;
-	action->action();
+
+	// We can't throw across a thread and *certainly* not across the api boundary
+	// But how do we deal with exceptions thrown in main thread callbacks if the caller doesn't wait for them?
+	// Likely the only good solution is abort()
+	try
+	{
+		action->action();
+	}
+	catch (const std::exception& e)
+	{
+		LogError("Exception in main thread handler: %s", e.what());
+		fprintf(stderr, "Exception in main thread handler: %s\n", e.what());
+		abort();
+	}
+	catch (...)
+	{
+		LogError("Exception in main thread handler: <unknown exception>");
+		fprintf(stderr, "Exception in main thread handler: <unknown exception>\n");
+		abort();
+	}
 	delete action;
 }
 
@@ -70,7 +90,14 @@ Ref<MainThreadAction> BinaryNinja::ExecuteOnMainThread(const function<void()>& a
 static void ExecuteActionLocal(void* ctxt)
 {
 	MainThreadActionContext* action = (MainThreadActionContext*)ctxt;
-	action->action();
+	try
+	{
+		action->action();
+	}
+	catch (...)
+	{
+		action->exception = current_exception();
+	}
 }
 
 
@@ -78,7 +105,12 @@ void BinaryNinja::ExecuteOnMainThreadAndWait(const function<void()>& action)
 {
 	MainThreadActionContext ctxt;
 	ctxt.action = action;
+	ctxt.exception = exception_ptr();
 	BNExecuteOnMainThreadAndWait(&ctxt, ExecuteActionLocal);
+	if (ctxt.exception)
+	{
+		rethrow_exception(ctxt.exception);
+	}
 }
 
 

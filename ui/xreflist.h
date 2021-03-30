3d4c8e147c5f6a6e257ef48cfb5fe476c35b1d7b
Josh Ferrell <josh@vector35.com>
2021-03-30 01:11:18 -0400

    Fix ExpandableGroup naming and animation

--- ui/xreflist.h
+++ ui/xreflist.h
@@ -517,13 +517,14 @@ private:
 	QScrollArea* m_content;
 	int m_duration = 100;
 
-public Q_SLOTS:
-	void toggle(bool collapsed);
+private Q_SLOTS:
+	void toggled(bool expanded);
 
 public:
 	explicit ExpandableGroup(const QString& title = "", QWidget* parent = nullptr);
 	void setContentLayout(QLayout* contentLayout);
 	void setTitle(const QString& title) { m_button->setText(title); }
+	void toggle(bool expanded);
 };
 
 

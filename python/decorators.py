a39ee992ab54eddb4ce06c41e8d552e55bf4e0b2
Jordan Wiens <jordan@psifertex.com>
2021-04-07 20:22:36 -0400

    good thing the decorator wasnt applied to anything with an actual pydoc

--- python/decorators.py
+++ python/decorators.py
@@ -4,7 +4,7 @@ def passive(cls):
 	.. note:: This object is a "passive" object. Any changes you make to it will not be reflected in the core and vice-versa. If you wish to update a core version of this object you should use the appropriate API.
 '''
 
-	if hasattr(cls, ".__doc__") and cls.__doc__:
+	if hasattr(cls, "__doc__") and cls.__doc__:
 		cls.__doc__ += passive_note
 	else:
 		cls.__doc__ = passive_note

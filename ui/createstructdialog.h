447dc5e66e7a5bdff9dbed299b46e229567a64ef
Xusheng <xusheng@vector35.com>
2021-05-04 16:22:57 +0800

    Parse a BinaryView into CreateStructDialog

--- ui/createstructdialog.h
+++ ui/createstructdialog.h
@@ -12,11 +12,12 @@ class BINARYNINJAUIAPI CreateStructDialog: public QDialog
 	QLineEdit* m_name;
 	QLineEdit* m_size;
 
+	BinaryViewRef m_view;
 	BinaryNinja::QualifiedName m_resultName;
 	uint64_t m_resultSize;
 
 public:
-	CreateStructDialog(QWidget* parent, const std::string& name);
+	CreateStructDialog(QWidget* parent, BinaryViewRef view, const std::string& name);
 
 	BinaryNinja::QualifiedName getName() { return m_resultName; }
 	uint64_t getSize() { return m_resultSize; }

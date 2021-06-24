e0389d244e6211126267e060a9c61a1e75481b76
Peter LaFosse <peter@vector35.com>
2021-06-24 08:24:47 -0400

    update unit test for preprocess_source change

--- suite/testcommon.py
+++ suite/testcommon.py
@@ -738,8 +738,7 @@ class TestBuilder(Builder):
                 long long bar1 = 2;
                 #endif
                 """)
-                source = '\n'.join([i.decode('charmap') for i in preprocessed[0].split(b'\n') if not b'#line' in i and len(i) > 0])
-                source = str(source) #TODO: remove when PY2 support has ended
+                source = '\n'.join([i for i in preprocessed[0].split('\n') if not '#line' in i and len(i) > 0])
                 typelist = bv.platform.parse_types_from_source(source)
                 inttype = binja.Type.int(4)
 

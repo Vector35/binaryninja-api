0d5ce53be355905aa9c1b3eae5c3ec8030613c7c
Jordan Wiens <jordan@psifertex.com>
2021-06-28 15:54:51 -0400

    support for section and segment offsets in copy menu

--- ui/addressindicator.h
+++ ui/addressindicator.h
@@ -11,11 +11,12 @@ class BINARYNINJAUIAPI AddressIndicator: public MenuHelper
 	Q_OBJECT
 
 	uint64_t m_begin, m_end;
+	BinaryViewRef m_view;
 
 public:
 	AddressIndicator(QWidget* parent);
 
-	void setOffsets(uint64_t begin, uint64_t end);
+	void setOffsets(uint64_t begin, uint64_t end, BinaryViewRef view);
 
 protected:
 	virtual void showMenu();

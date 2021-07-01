421b398453d09e06e7b202229e34b66414683a45
Peter LaFosse <peter@vector35.com>
2021-07-01 09:25:13 -0400

    Fix bug in python/variable.py

--- python/variable.py
+++ python/variable.py
@@ -723,7 +723,7 @@ class Variable:
 	@property
 	def type(self) -> 'binaryninja.types.Type':
 		var_type_conf = core.BNGetVariableType(self._function.handle, self._var.to_BNVariable())
-		assert var_type_conf.type
+		assert var_type_conf.type is not None, "core.BNGetVariableType returned None"
 		_type = binaryninja.types.Type(var_type_conf.type, self._function.platform, var_type_conf.confidence)
 		return _type
 

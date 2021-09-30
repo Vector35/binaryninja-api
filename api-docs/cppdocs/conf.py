0697899324fb41d92cc1179a7b0035abdf0bb4a3
Jordan Wiens <jordan@psifertex.com>
2021-09-30 14:11:28 -0400

    remove sphinx submodule

--- api-docs/cppdocs/conf.py
+++ api-docs/cppdocs/conf.py
@@ -113,8 +113,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [os.path.join(os.path.abspath("."), "..", "..", "sphinx_rtd_theme")]
+html_theme = 'sphinx_rtd'
+html_theme_path = [os.path.join(os.path.abspath("."), "..", "..")]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

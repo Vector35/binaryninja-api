6b505fb40c4dfcf041dede6ba0f0f60c59e73ed0
rose <47357290+rompse@users.noreply.github.com>
2021-10-05 17:20:51 -0400

    TriageView: path, size, md5, sha1, sha256

---
+++ examples/triage/fileinfo.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QWidget>
+#include <QCryptographicHash>
+#include "uitypes.h"
+#include "viewframe.h"
+
+class FileInfoWidget: public QWidget
+{
+    static constexpr std::int32_t m_maxColumns{2};
+    std::pair<std::int32_t, std::int32_t> m_fieldPosition{}; // row, column
+    QGridLayout* m_layout{};
+
+    void addField(const QString& name, const QVariant& value);
+    void addHashField(const QString& hashName,
+                      const QCryptographicHash::Algorithm& algorithm,
+                      const QByteArray& data);
+
+public:
+    FileInfoWidget(QWidget* parent, BinaryViewRef bv);
+};

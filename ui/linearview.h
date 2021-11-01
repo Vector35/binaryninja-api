00ba1e56231c909d91fb100464d4ccb72b22c005
Andrew Lamoureux <andrew@vector35.com>
2021-11-01 09:50:20 -0400

    Add getSettings() to linearview

--- ui/linearview.h
+++ ui/linearview.h
@@ -303,6 +303,7 @@ public:
 
 	virtual BinaryViewRef getData() override { return m_data; }
 	void getCurrentOffsetByType(TypeRef resType, uint64_t baseAddr, uint64_t& begin, uint64_t& end, bool singleLine);
+	virtual DisassemblySettingsRef getSettings();
 	virtual uint64_t getCurrentOffset() override;
 	virtual UIActionContext actionContext() override;
 	virtual BNAddressRange getSelectionOffsets() override;

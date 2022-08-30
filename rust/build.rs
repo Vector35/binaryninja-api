568050e3cf5e96e134931181de14885e5b64d735
KyleMiles <krm504@nyu.edu>
2022-08-30 11:23:35 -0400

    Rust doc : Copy images every time, not just when the images change

--- rust/build.rs
+++ rust/build.rs
@@ -1,8 +1,4 @@
 fn main() {
-    println!("cargo:rerun-if-changed=under_construction.png");
-    println!("cargo:rerun-if-changed=../docs/img/favicon.ico");
-    println!("cargo:rerun-if-changed=../docs/img/logo.png");
-
     let _ = std::fs::create_dir("target/doc");
     let _ = std::fs::copy("../docs/img/favicon.ico", "target/doc/favicon.ico");
     let _ = std::fs::copy(

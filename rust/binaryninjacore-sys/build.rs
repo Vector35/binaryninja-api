bd78fe4b68031ceccca3365ab4fc7c3333cc961c
Xusheng <xusheng@vector35.com>
2023-08-01 14:55:19 +0800

    Fix demo build issue on Linux due to failure to create symlink for libbinaryninjacore.so.1

--- rust/binaryninjacore-sys/build.rs
+++ rust/binaryninjacore-sys/build.rs
@@ -61,7 +61,7 @@ fn main() {
     #[cfg(target_os = "linux")]
     {
         let symlink_target = PathBuf::from(&out_dir).join("libbinaryninjacore.so");
-        if !link_path.join("libbinaryninjacore.so").exists() && !symlink_target.exists() {
+        if link_path.join("libbinaryninjacore.so.1").exists() && !symlink_target.exists() {
             use std::os::unix::fs;
             fs::symlink(
                 link_path.join("libbinaryninjacore.so.1"),

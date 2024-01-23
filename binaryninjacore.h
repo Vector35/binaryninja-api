2d45ae4ea90d0792627927def462ed7c2350bb89
Alexander Taylor <alex@vector35.com>
2024-01-23 13:00:05 -0500

    Bump ABI version due to projects changes.

--- binaryninjacore.h
+++ binaryninjacore.h
@@ -37,14 +37,14 @@
 // Current ABI version for linking to the core. This is incremented any time
 // there are changes to the API that affect linking, including new functions,
 // new types, or modifications to existing functions or types.
-#define BN_CURRENT_CORE_ABI_VERSION 46
+#define BN_CURRENT_CORE_ABI_VERSION 47
 
 // Minimum ABI version that is supported for loading of plugins. Plugins that
 // are linked to an ABI version less than this will not be able to load and
 // will require rebuilding. The minimum version is increased when there are
 // incompatible changes that break binary compatibility, such as changes to
 // existing types or functions.
-#define BN_MINIMUM_CORE_ABI_VERSION 45
+#define BN_MINIMUM_CORE_ABI_VERSION 47
 
 #ifdef __GNUC__
 	#ifdef BINARYNINJACORE_LIBRARY

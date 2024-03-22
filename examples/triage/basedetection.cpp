c3a6ad8d6bdbcc0532812439c7356832a1495678
Brandon Miller <brandon@vector35.com>
2024-03-22 08:56:07 -0400

    Running in QThread from base detection widget
    Kicking off analysis in a worker thread from the base detection QT
    widget. Wired in start of callback to handle results and update the
    UI when analysis completes

--- examples/triage/basedetection.cpp
+++ examples/triage/basedetection.cpp
@@ -3,35 +3,92 @@
 using namespace BinaryNinja;
 using namespace std;
 
-BaseDetection::BaseDetection(BinaryViewRef bv, std::string arch, std::string analysis, uint32_t minStr, uint32_t pageSize)
+BaseDetection::BaseDetection(BinaryViewRef bv, BaseDetectionSettings& settings)
 {
-	this->m_view = bv->GetParentView() ? bv->GetParentView() : bv;
+	m_view = bv->GetFile()->GetViewOfType("Raw");
 	m_logger = LogRegistry::CreateLogger("TriageView.BaseDetection");
+	m_settings = settings;
 	m_logger->LogDebug(
-		"Base Address Detection:\n"
+		"BaseDetection:\n"
 		"\tArchitecture:       %s\n"
 		"\tAnalysis Level:     %s\n"
 		"\tMin. String Length: %d\n"
 		"\tPage Size:          %08x",
-		arch.c_str(),
-		analysis.c_str(),
-		minStr,
-		pageSize
+		m_settings.Architecture.c_str(),
+		m_settings.Analysis.c_str(),
+		m_settings.MinStrlen,
+		m_settings.PageSize
 	);
+}
+
+void BaseDetection::runAnalysis()
+{
+	auto loadSettings = Settings::Instance();
+	map<string, Ref<Metadata>> metadataMap =
+	{
+		{"loader.imageBase", new Metadata((uint64_t) 0)},
+		{"analysis.mode", new Metadata(m_settings.Analysis)},
+		{"analysis.limits.minStringLength", new Metadata((uint64_t) m_settings.MinStrlen)},
+		{"analysis.linearSweep.permissive", new Metadata(true)}
+	};
+	if (m_settings.Architecture != "auto detect")
+		metadataMap["loader.architecture"] = new Metadata(m_settings.Architecture);
+	Ref<Metadata> options = new Metadata(metadataMap);
+	m_view = Load(m_view, true, nullptr, options);
+}
+
+void BaseDetection::identifyPointsOfInterest()
+{
+	if (!m_view || !m_view->HasInitialAnalysis())
+	{
+		m_logger->LogError("BaseDetection: view is not initialized!?");
+		return;
+	}
+
+	for (const auto& func : m_view->GetAnalysisFunctionList())
+	{
+		m_funcOffsets.insert(func->GetStart());
+	}
+
+	m_logger->LogDebug("BaseDetection: identified %d functions", m_funcOffsets.size());
+}
+
+void BaseDetection::DetectBaseAddress()
+{
+	this->runAnalysis();
+	this->identifyPointsOfInterest();
+}
+
+void BaseDetectionThread::run()
+{
+	QString result; // TODO - final results will be more than a string
+
+	// TODO: sanatize inputs (handle when users input strings for ints, etc..)
+	BaseDetectionSettings settings = {
+		m_inputs->ArchitectureBox->currentText().toStdString(),
+		m_inputs->AnalysisBox->currentText().toStdString(),
+		m_inputs->StrlenLineEdit->text().toInt(),
+		m_inputs->PageSizeLineEdit->text().toInt()
+	};
+
+	auto baseDetection = BaseDetection(m_view, settings);
+	baseDetection.DetectBaseAddress();
+	emit resultReady(result);
+}
+
+void BaseDetectionWidget::handleResults(const QString& result)
+{
+	m_detectBaseAddressButton->setEnabled(true);
+	// TODO: handle results and update the UI
 }
 
 void BaseDetectionWidget::detectBaseAddress()
 {
 	m_detectBaseAddressButton->setEnabled(false);
-
-	// TODO: sanitize the inputs and support hex integers prefixed by 0x
-	BaseDetection(
-		this->m_view,
-		m_inputs->ArchitectureBox->currentText().toStdString(),
-		m_inputs->AnalysisBox->currentText().toStdString(),
-		m_inputs->StrlenLineEdit->text().toInt(),
-		m_inputs->PageSizeLineEdit->text().toInt()
-	);
+	auto workerThread = new BaseDetectionThread(&m_inputs, m_view);
+	connect(workerThread, &BaseDetectionThread::resultReady, this, &BaseDetectionWidget::handleResults);
+	connect(workerThread, &BaseDetectionThread::finished, workerThread, &QObject::deleteLater);
+	workerThread->start();
 }
 
 BaseDetectionWidget::BaseDetectionWidget(QWidget* parent, BinaryViewRef bv)
@@ -40,30 +97,29 @@ BaseDetectionWidget::BaseDetectionWidget(QWidget* parent, BinaryViewRef bv)
 	this->m_layout = new QGridLayout();
 	auto& [row, column] = this->m_fieldPosition;
 
-	m_inputs = new BaseDetectionWidgetInputs();
 	this->m_layout->addWidget(new QLabel("Architecture:"), row, column);
-	m_inputs->ArchitectureBox = new QComboBox(this);
+	m_inputs.ArchitectureBox = new QComboBox(this);
 	auto archItemList = QStringList();
 	archItemList << "auto detect";
 	auto architectures = Architecture::GetList();
 	for (const auto& arch : architectures)
 		archItemList << QString::fromStdString(arch->GetName());
-	m_inputs->ArchitectureBox->addItems(archItemList);
-	this->m_layout->addWidget(m_inputs->ArchitectureBox, row++, column + 1);
+	m_inputs.ArchitectureBox->addItems(archItemList);
+	this->m_layout->addWidget(m_inputs.ArchitectureBox, row++, column + 1);
 
 	this->m_layout->addWidget(new QLabel("Analysis Level:"), row, column);
-	m_inputs->AnalysisBox = new QComboBox(this);
+	m_inputs.AnalysisBox = new QComboBox(this);
 	auto analysisItemList = QStringList() << "basic" << "controlFlow" << "full";
-	m_inputs->AnalysisBox->addItems(analysisItemList);
-	this->m_layout->addWidget(m_inputs->AnalysisBox, row++, column + 1);
+	m_inputs.AnalysisBox->addItems(analysisItemList);
+	this->m_layout->addWidget(m_inputs.AnalysisBox, row++, column + 1);
 
 	this->m_layout->addWidget(new QLabel("Min. String Length:"), row, column);
-	m_inputs->StrlenLineEdit = new QLineEdit("10");
-	this->m_layout->addWidget(m_inputs->StrlenLineEdit, row++, column + 1);
+	m_inputs.StrlenLineEdit = new QLineEdit("10");
+	this->m_layout->addWidget(m_inputs.StrlenLineEdit, row++, column + 1);
 
 	this->m_layout->addWidget(new QLabel("Page Size:"), row, column);
-	m_inputs->PageSizeLineEdit = new QLineEdit("1000");
-	this->m_layout->addWidget(m_inputs->PageSizeLineEdit, row++, column + 1);
+	m_inputs.PageSizeLineEdit = new QLineEdit("1000");
+	this->m_layout->addWidget(m_inputs.PageSizeLineEdit, row++, column + 1);
 
 	m_detectBaseAddressButton = new QPushButton("Start Detection");
 	connect(m_detectBaseAddressButton, &QPushButton::clicked, this, &BaseDetectionWidget::detectBaseAddress);
--- examples/triage/basedetection.h
+++ examples/triage/basedetection.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <QThread>
 #include <QtWidgets/QWidget>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QLineEdit>
@@ -7,23 +8,74 @@
 #include "viewframe.h"
 #include "binaryninjaapi.h"
 
+struct BaseDetectionSettings
+{
+	std::string Architecture;
+	std::string Analysis;
+	int MinStrlen;
+	int PageSize;
+};
+
 namespace BinaryNinja
 {
-	class BaseDetection {
+	class BaseDetection
+	{
 		BinaryViewRef m_view;
-		Ref<Logger> m_logger;	
+		BaseDetectionSettings m_settings;
+		bool m_wait;
+		Ref<Logger> m_logger;
 
-		public:
-			BaseDetection(BinaryViewRef bv, std::string arch, std::string analysis, uint32_t minStr, uint32_t pageSize);
+		// Points of interest
+		std::set<uint64_t> m_stringOffsets;
+		std::set<uint64_t> m_funcOffsets;
+		std::set<uint64_t> m_dataVariableOffsets;
+
+		// Identified pointer values
+		std::set<uint64_t> m_pointers;
+
+		static inline bool m_abort {false};
+		static inline std::mutex m_mutex {};
+
+		void runAnalysis();
+		bool identifyPointers();
+		void identifyPointsOfInterest();
+
+	public:
+		void AbortAnalysis() { m_abort = true; }
+		static bool AnalysisProgress(size_t complete, size_t total)
+		{
+			return true;
+		}
+
+		BaseDetection(BinaryViewRef bv, BaseDetectionSettings& settings);
+		void DetectBaseAddress();
 	};
 }
 
-class BaseDetectionWidgetInputs {
-	public:
-		QComboBox* ArchitectureBox;
-		QComboBox* AnalysisBox;
-		QLineEdit* StrlenLineEdit;
-		QLineEdit* PageSizeLineEdit;
+struct BaseDetectionQtInputs
+{
+	QComboBox* ArchitectureBox;
+	QComboBox* AnalysisBox;
+	QLineEdit* StrlenLineEdit;
+	QLineEdit* PageSizeLineEdit;
+};
+
+class BaseDetectionThread : public QThread
+{
+	Q_OBJECT
+	BinaryViewRef m_view;
+	BaseDetectionQtInputs* m_inputs {};
+	void run() override;
+
+public:
+	BaseDetectionThread(BaseDetectionQtInputs* widgetInputs, BinaryViewRef bv)
+	{
+		m_inputs = widgetInputs;
+		m_view = bv;
+	}
+
+signals:
+	void resultReady(const QString& result);
 };
 
 class BaseDetectionWidget : public QWidget
@@ -32,12 +84,11 @@ class BaseDetectionWidget : public QWidget
 	std::pair<std::int32_t, std::int32_t> m_fieldPosition {};  // row, column
 	QGridLayout* m_layout {};
 	QPushButton* m_detectBaseAddressButton = nullptr;
-
-	BaseDetectionWidgetInputs* m_inputs {};
+	BaseDetectionQtInputs m_inputs;
 	BinaryViewRef m_view;
-
 	void detectBaseAddress();
+	void handleResults(const QString& result);
 
-	public:
-		BaseDetectionWidget(QWidget* parent, BinaryViewRef bv);
+public:
+	BaseDetectionWidget(QWidget* parent, BinaryViewRef bv);
 };

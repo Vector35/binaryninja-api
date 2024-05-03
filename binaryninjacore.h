5363ba136b59019f4f3ff78eb977ddc69132eca9
Mason Reed <mason@vector35.com>
2024-05-03 11:27:00 -0400

    Bump ABI to 61

--- binaryninjacore.h
+++ binaryninjacore.h
@@ -37,7 +37,7 @@
 // Current ABI version for linking to the core. This is incremented any time
 // there are changes to the API that affect linking, including new functions,
 // new types, or modifications to existing functions or types.
-#define BN_CURRENT_CORE_ABI_VERSION 60
+#define BN_CURRENT_CORE_ABI_VERSION 61
 
 // Minimum ABI version that is supported for loading of plugins. Plugins that
 // are linked to an ABI version less than this will not be able to load and

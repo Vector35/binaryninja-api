466fe3d9bdb98467a13bd8f291ced4cb94bd79ab
Michael Krasnitski <michael.krasnitski@gmail.com>
2024-05-09 21:07:01 -0400

    Add `AbstractType` trait

--- rust/src/types.rs
+++ rust/src/types.rs
@@ -695,6 +695,41 @@ impl Drop for TypeBuilder {
 //////////
 // Type
 
+pub trait AbstractType {
+    fn resolve_type() -> Ref<Type>;
+}
+
+macro_rules! abstract_type {
+    ($($t:ty => $e:expr),+) => {
+        $(
+            impl AbstractType for $t {
+                fn resolve_type() -> Ref<Type> {
+                    $e
+                }
+            }
+        )+
+    }
+}
+
+abstract_type! {
+    u8 => Type::int(1, false),
+    u16 => Type::int(2, false),
+    u32 => Type::int(4, false),
+    u64 => Type::int(8, false),
+    i8 => Type::int(1, true),
+    i16 => Type::int(2, true),
+    i32 => Type::int(4, true),
+    i64 => Type::int(8, true),
+    f32 => Type::float(4),
+    f64 => Type::float(8)
+}
+
+impl<T: AbstractType, const N: usize> AbstractType for [T; N] {
+    fn resolve_type() -> Ref<Type> {
+        Type::array(&T::resolve_type(), N as u64)
+    }
+}
+
 #[repr(transparent)]
 pub struct Type {
     pub(crate) handle: *mut BNType,

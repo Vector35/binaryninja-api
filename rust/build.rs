ab06d72c6e35a9a2fdd0bd0ce28f8946b67ea233
Mason Reed <mason@vector35.com>
2024-07-29 11:30:26 -0400

    Link core in rust
    When linking you must depend on the -sys crate

--- rust/build.rs
+++ rust/build.rs
@@ -9,4 +9,18 @@ fn main() {
         "target/doc/under_construction.png",
     );
     let _ = std::fs::copy("../docs/img/logo.png", "target/doc/logo.png");
+    
+    let link_path =
+        std::env::var_os("DEP_BINARYNINJACORE_PATH").expect("DEP_BINARYNINJACORE_PATH specified");
+
+    println!("cargo::rustc-link-lib=dylib=binaryninjacore");
+    println!("cargo::rustc-link-search={}", link_path.to_str().unwrap());
+
+    #[cfg(not(target_os = "windows"))]
+    {
+        println!(
+            "cargo::rustc-link-arg=-Wl,-rpath,{0},-L{0}",
+            link_path.to_string_lossy()
+        );
+    }
 }

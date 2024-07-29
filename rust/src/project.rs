995ec20ef0bd762d189ad52e75b03d523e92b96a
Mason Reed <mason@vector35.com>
2024-07-29 11:32:49 -0400

    Canonicalize path in `create_delete_empty` test

--- rust/src/project.rs
+++ rust/src/project.rs
@@ -1143,6 +1143,7 @@ mod test {
 
     #[test]
     fn create_delete_empty() {
+        use std::fs::canonicalize;
         crate::headless::init();
 
         let (project_path, project_name) = unique_project();
@@ -1153,7 +1154,10 @@ mod test {
 
         // check project data
         let project_path_received = project.path();
-        assert_eq!(&project_path, project_path_received.as_str());
+        assert_eq!(
+            canonicalize(&project_path).unwrap(),
+            canonicalize(project_path_received.to_string()).unwrap()
+        );
         let project_name_received = project.name();
         assert_eq!(&project_name, project_name_received.as_str());
 

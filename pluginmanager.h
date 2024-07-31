25856e31c1bcb14ea3eefb70d0f0c9f60ac053eb
Josh Ferrell <josh@vector35.com>
2024-07-31 13:38:22 -0400

    Split out PluginManager

---
+++ pluginmanager.h
@@ -0,0 +1,100 @@
+#pragma once
+
+#include "binaryninjacore.h"
+#include "refcount.h"
+#include <string>
+#include <vector>
+
+namespace BinaryNinja
+{
+
+	typedef BNPluginOrigin PluginOrigin;
+	typedef BNPluginStatus PluginStatus;
+	typedef BNPluginType PluginType;
+
+	/*!
+		\ingroup pluginmanager
+	*/
+	class RepoPlugin : public CoreRefCountObject<BNRepoPlugin, BNNewPluginReference, BNFreePlugin>
+	{
+	  public:
+		RepoPlugin(BNRepoPlugin* plugin);
+		PluginStatus GetPluginStatus() const;
+		std::vector<std::string> GetApis() const;
+		std::vector<std::string> GetInstallPlatforms() const;
+		std::string GetPath() const;
+		std::string GetSubdir() const;
+		std::string GetDependencies() const;
+		std::string GetPluginDirectory() const;
+		std::string GetAuthor() const;
+		std::string GetDescription() const;
+		std::string GetLicenseText() const;
+		std::string GetLongdescription() const;
+		std::string GetName() const;
+		std::vector<PluginType> GetPluginTypes() const;
+		std::string GetPackageUrl() const;
+		std::string GetProjectUrl() const;
+		std::string GetAuthorUrl() const;
+		std::string GetVersion() const;
+		std::string GetCommit() const;
+		std::string GetRepository() const;
+		std::string GetProjectData();
+		BNVersionInfo GetMinimumVersionInfo() const;
+		BNVersionInfo GetMaximumVersionInfo() const;
+		uint64_t GetLastUpdate();
+		bool IsBeingDeleted() const;
+		bool IsBeingUpdated() const;
+		bool IsInstalled() const;
+		bool IsEnabled() const;
+		bool IsRunning() const;
+		bool IsUpdatePending() const;
+		bool IsDisablePending() const;
+		bool IsDeletePending() const;
+		bool IsUpdateAvailable() const;
+		bool AreDependenciesBeingInstalled() const;
+
+		bool Uninstall();
+		bool Install();
+		bool InstallDependencies();
+		// `force` ignores optional checks for platform/api compliance
+		bool Enable(bool force);
+		bool Disable();
+		bool Update();
+	};
+
+	/*!
+		\ingroup pluginmanager
+	*/
+	class Repository : public CoreRefCountObject<BNRepository, BNNewRepositoryReference, BNFreeRepository>
+	{
+	  public:
+		Repository(BNRepository* repository);
+		std::string GetUrl() const;
+		std::string GetRepoPath() const;
+		std::string GetLocalReference() const;
+		std::string GetRemoteReference() const;
+		std::vector<Ref<RepoPlugin>> GetPlugins() const;
+		std::string GetPluginDirectory() const;
+		Ref<RepoPlugin> GetPluginByPath(const std::string& pluginPath);
+		std::string GetFullPath() const;
+	};
+
+	/*!
+		\ingroup pluginmanager
+	*/
+	class RepositoryManager :
+	    public CoreRefCountObject<BNRepositoryManager, BNNewRepositoryManagerReference, BNFreeRepositoryManager>
+	{
+	  public:
+		RepositoryManager(const std::string& enabledPluginsPath);
+		RepositoryManager(BNRepositoryManager* repoManager);
+		RepositoryManager();
+		bool CheckForUpdates();
+		std::vector<Ref<Repository>> GetRepositories();
+		Ref<Repository> GetRepositoryByPath(const std::string& repoName);
+		bool AddRepository(const std::string& url,  // URL to raw plugins.json file
+		    const std::string& repoPath);           // Relative path within the repositories directory
+		Ref<Repository> GetDefaultRepository();
+	};
+
+}

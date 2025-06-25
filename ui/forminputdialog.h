93b26938d1bf171bf335dcf8976e50acc0860d08
Hongyu Chen <hongyu@vector35.com>
2025-06-25 12:52:17 -0400

    [ui] Updated forminputdialog.h to support callbacks in BINARYNINJA/ui
    This commit is related to Issues#6948

--- ui/forminputdialog.h
+++ ui/forminputdialog.h
@@ -20,13 +20,28 @@ class BINARYNINJAUIAPI FormInputDialog : public QDialog
 	std::vector<BinaryNinja::FormInputField>* m_fields;
 	std::vector<QWidget*> m_fieldControls;
 
+	public:
 	void openFileName(QLineEdit* edit, const std::string& ext);
 	void saveFileName(QLineEdit* edit, const std::string& ext, const std::string& defaultName);
 	void directoryName(QLineEdit* edit, const std::string& defaultName);
 
-  public:
 	FormInputDialog(QWidget* parent, std::vector<BinaryNinja::FormInputField>* fields, const std::string& title);
+	// Callback function types
+    typedef bool (*OpenFileNameCallback)(std::string& result, const std::string& ext);
+    typedef bool (*SaveFileNameCallback)(std::string& result, const std::string& ext, const std::string& defaultName);
+    typedef bool (*DirectoryNameCallback)(std::string& result, const std::string& defaultName);
 
+    // Static callback pointers (will be set by main executable)
+    static OpenFileNameCallback s_openFileNameCallback;
+    static SaveFileNameCallback s_saveFileNameCallback;
+    static DirectoryNameCallback s_directoryNameCallback;
+
+    // Registration function (called by main executable during startup)
+    static void registerFileDialogCallbacks(
+        OpenFileNameCallback openFileCallback,
+        SaveFileNameCallback saveFileCallback,
+        DirectoryNameCallback directoryCallback
+    );
   protected Q_SLOTS:
 	void finish();
 };

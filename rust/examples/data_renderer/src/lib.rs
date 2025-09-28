c0e7d2bc30ef90b96064379f12265571c9b491b4
LukBukkit <luk.bukkit@gmail.com>
2025-09-28 17:55:51 +0200

    Adds data renderer UUID example

---
+++ rust/examples/data_renderer/src/lib.rs
@@ -0,0 +1,108 @@
+use binaryninja::binary_view::{BinaryView, BinaryViewBase};
+use binaryninja::data_renderer::{
+    CustomDataRenderer, TypeContext, register_specific_data_renderer,
+};
+use binaryninja::disassembly::{
+    DisassemblyTextLine, InstructionTextToken, InstructionTextTokenKind,
+};
+use binaryninja::types::{Type, TypeClass};
+use log::debug;
+use uuid::Uuid;
+
+struct UuidDataRenderer {}
+
+impl CustomDataRenderer for UuidDataRenderer {
+    fn is_valid_for_data(
+        &self,
+        view: &BinaryView,
+        addr: u64,
+        type_: &Type,
+        types: &[TypeContext],
+    ) -> bool {
+        // We only want to render arrays with a size of 16 elements
+        if type_.type_class() != TypeClass::ArrayTypeClass {
+            return false;
+        }
+        if type_.count() != 0x10 {
+            return false;
+        }
+
+        // The array elements must be of the type uint8_t
+        let Some(element_type_conf) = type_.element_type() else {
+            return false;
+        };
+        let element_type = element_type_conf.contents;
+        if element_type.type_class() != TypeClass::IntegerTypeClass {
+            return false;
+        }
+        if element_type.width() != 1 {
+            return false;
+        }
+
+        // The array should be embedded in a named type reference with the id macho:["uuid"]
+        for type_ctx in types {
+            if type_ctx.type_().type_class() != TypeClass::NamedTypeReferenceClass {
+                continue;
+            }
+
+            let Some(name_ref) = type_ctx.type_().get_named_type_reference() else {
+                continue;
+            };
+
+            if name_ref.id() == "macho:[\"uuid\"]" {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    fn lines_for_data(
+        &self,
+        view: &BinaryView,
+        addr: u64,
+        type_: &Type,
+        prefix: Vec<InstructionTextToken>,
+        width: usize,
+        types_ctx: &[TypeContext],
+        language: &str,
+    ) -> Vec<DisassemblyTextLine> {
+        let mut tokens = prefix.clone();
+
+        let mut buf = [0u8; 0x10];
+        let bytes_read = view.read(&mut buf, addr);
+
+        // Make sure that we've read all UUID bytes and convert them to token
+        if bytes_read == 0x10 {
+            tokens.extend([
+                InstructionTextToken::new("UUID(\"", InstructionTextTokenKind::Text),
+                InstructionTextToken::new(
+                    Uuid::from_bytes(buf).to_string(),
+                    InstructionTextTokenKind::String { value: 0 },
+                ),
+                InstructionTextToken::new("\")", InstructionTextTokenKind::Text),
+            ]);
+        } else {
+            tokens.push(InstructionTextToken::new(
+                "error: cannot read 0x10 bytes",
+                InstructionTextTokenKind::Annotation,
+            ));
+        }
+
+        vec![DisassemblyTextLine::new_with_addr(tokens, addr)]
+    }
+}
+
+#[allow(non_snake_case)]
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn CorePluginInit() -> bool {
+    // Initialize logging
+    binaryninja::logger::Logger::new("UUID Data Renderer")
+        .with_level(log::LevelFilter::Debug)
+        .init();
+
+    // Register data renderer
+    register_specific_data_renderer(UuidDataRenderer {});
+
+    true
+}

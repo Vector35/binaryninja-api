efa9d4765e4f1b2ddcd61a8e15af99392dd3a605
Glenn Smith <glenn@vector35.com>
2026-05-18 17:24:48 -0400

    Mark created files as ready when they are ready

--- ui/util.h
+++ ui/util.h
@@ -37,6 +37,7 @@ std::optional<BinaryNinja::PossibleValueSet> BINARYNINJAUIAPI getPossibleValueSe
 std::optional<BinaryNinja::PossibleValueSet> BINARYNINJAUIAPI getPossibleValueSetForILToken(View* view, HighlightTokenState token);
 std::optional<uint64_t> BINARYNINJAUIAPI getAddressOfILTokenExpr(View* view, HighlightTokenState token);
 void BINARYNINJAUIAPI setCallStackAdjustment(QWidget* parent, FunctionRef func, ArchitectureRef arch, uint64_t instrAddress);
+bool BINARYNINJAUIAPI markProjectFileReadyForPath(const QString& path);
 
 // Resolve the address of the call instruction that the user is currently on,
 // given the active function, architecture, IL view, highlight, and cursor

65bfc8c57c14641c3f476933fd4dd0151cabaf93
Peter LaFosse <peter@vector35.com>
2026-09-24 20:15:02 -0400

    Lower GNU demangler nesting limit for worker stacks

--- demangler/gnu3/demangle_gnu3.h
+++ demangler/gnu3/demangle_gnu3.h
@@ -94,7 +94,9 @@ private:
 	using NodeRefList = _STD_VECTOR<NodeRef>;
 
 	static constexpr size_t MAX_DEMANGLE_NODE_LENGTH = 8192;
-	static constexpr size_t MAX_DEMANGLE_NESTING_DEPTH = 1024;
+	// Analysis workers can have 512 KiB stacks. Each grammar level can use
+	// several parser and type-node frames, so leave room for finalization.
+	static constexpr size_t MAX_DEMANGLE_NESTING_DEPTH = 128;
 	_STD_STRING m_mangledName;
 	DemangleReader m_reader{m_mangledName, MAX_DEMANGLE_NODE_LENGTH, false};
 	std::reference_wrapper<BN::Platform> m_platform;
